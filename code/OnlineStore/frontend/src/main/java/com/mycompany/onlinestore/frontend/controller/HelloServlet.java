e121637df4228a8e1373544293f43c6fc0fd33e1
Grassien <fredgsn59@gtmail.com>
2022-03-08 18:28:33 +0100

    Update 08-03

--- code/OnlineStore/frontend/src/main/java/com/mycompany/onlinestore/frontend/controller/HelloServlet.java
+++ code/OnlineStore/frontend/src/main/java/com/mycompany/onlinestore/frontend/controller/HelloServlet.java
@@ -1,8 +1,6 @@
-package controller;
+package com.mycompany.onlinestore.frontend.controller;
 
 import com.mycompany.backend.entity.Person;
-
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,19 +8,21 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 @WebServlet(name = "HelloServlet", urlPatterns = ("/hello"))
-public class HomeServlet extends HttpServlet {
+public class HelloServlet extends HttpServlet {
+    private String message;
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse resp) throws IOException {
         Person durand = new Person("David", "Dupond");
 
         PrintWriter out = resp.getWriter(); // ouverture flux en écriture
-
+        resp.setContentType("text/html");
+        
         String message = "Hello " + durand.getFirstname() + " " + durand.getLastname();
-
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
+
 }
+
--- code/OnlineStore/frontend/src/main/java/com/mycompany/onlinestore/frontend/controller/HomeServlet.java
+++ code/OnlineStore/frontend/src/main/java/com/mycompany/onlinestore/frontend/controller/HomeServlet.java
@@ -1,6 +1,8 @@
-package controller;
+package com.mycompany.onlinestore.frontend.controller;
 
 import com.mycompany.backend.entity.Person;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,21 +10,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+
 @WebServlet(name = "HelloServlet", urlPatterns = ("/hello"))
-public class HelloServlet extends HttpServlet {
-    private String message;
+public class HomeServlet extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Person durand = new Person("David", "Dupond");
 
         PrintWriter out = resp.getWriter(); // ouverture flux en écriture
-        resp.setContentType("text/html");
-        
+
         String message = "Hello " + durand.getFirstname() + " " + durand.getLastname();
+
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
-
 }
-
